8bf9c8ecc7f281dd31e9424e0610b7808aa29534
ZHIBIN ZHOU <56317282+zhibinz2@users.noreply.github.com>
2025-06-23 12:40:17 -0700

    corr

--- EEG_CORR_H.m
+++ EEG_CORR_H.m
@@ -2,12 +2,39 @@
 % open /home/zhibinz2/Documents/GitHub/Motor_coordination_code/Fig7_bootstrap_plot.m
 csyn2condi4; % 3nx x 2 syn x 2 condi (follow and mutual) x 7 freq x 32 chan
 cd /home/zhibinz2/Documents/GitHub/Motor_coordination_code
+load('nx144.mat'); % 144 trials  x 2 subjects x 7 freq x 32 chan
+% dg_ctr144;
+% efficiency144;
+% bw_ctr144;
+
 load('cc3_syn.mat'); %3nx x 2 syn x 4 condi x36 tr x 7 freq x 32 chan
-btwAB=cc3_syn(3,:,3:4,:,:,:); % btw nx x 2syn x 2 condi(follow, mutual) x 36 tr x 7 freq x 32 chan
+btwAB=squeeze(cc3_syn(3,:,3:4,:,:,:)); % btw nx x 2syn x 2 condi(follow, mutual) x 36 tr x 7 freq x 32 chan
 corr_btwAB=nan(2,2,7,32);
-
-
+for syn=1:2
+    for condi=1:2
+        for freq=1:7
+            for chan=1:32
+                A=squeeze(btwAB(syn,condi,1:18,freq,chan));
+                B=squeeze(btwAB(syn,condi,19:36,freq,chan));
+                corr_btwAB(syn,condi,freq,chan)=corr(A,B);
+            end
+        end
+    end
+end
 
 
 %% Correlate betweenness centrality to complexity score
-
+load('H_syn.mat');
+H_syn;% 2 syn x 4 condi x 36 tr
+corr_btw_H=nan(2,2,7,32);
+for syn=1:2
+    for condi=1:2
+        H_tmp=H_syn{syn}{condi+2}';
+        for freq=1:7
+            for chan=1:32
+                btw_tmp=squeeze(btwAB(syn,condi,:,freq,chan));
+                corr_btw_H(syn,condi,freq,chan)=corr(H_tmp,btw_tmp);
+            end
+        end
+    end
+end
